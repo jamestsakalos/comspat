4ac433c40a9ab45cbb0ea62bae3ea9b1b8db55ee
James Tsakalos <73455147+jamestsakalos@users.noreply.github.com>
2022-03-02 15:25:42 +0100

    Adding package importFrom / updating namespace

--- R/import_packages.r
+++ R/import_packages.r
@@ -1,4 +1,5 @@
-#' @importFrom graphics axis lines mtext par
+#' @importFrom graphics axis lines mtext par polygon
+#' @importFrom grDevices adjustcolor
 #' @importFrom stats aggregate na.omit sd
 #' @importFrom vdiffr expect_doppelganger
 #' @import knitr
